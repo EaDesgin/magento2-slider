be6efd4548e2c97df7198e4cf1c916a8b3a9f53f
Livia Baltatescu <livia@eadesign.ro>
2018-01-17 13:26:21 +0200

    Modificari versiune slider reg

--- registration.php
+++ registration.php
@@ -5,6 +5,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Eadesigndev_Slider',
+    'CzoneTech_Slider',
     __DIR__
 );
